f44749fc611fec243ce073516bbbbdb502d7a371
vincios <vinciosdev@gmail.com>
2025-01-22 15:05:40 +0100

    Add command-line interface to automatic_cot.py for testing; fix max_token parameter

--- automatic_cot.py
+++ automatic_cot.py
@@ -1,3 +1,4 @@
+import argparse
 import openai
 
 def generate_cot(model, cot_prompt):
@@ -11,7 +12,7 @@ def generate_cot(model, cot_prompt):
             model=model,
             messages=[{"role": "system", "content": prompt}],
             temperature=1,
-            max_tokens=4,
+            max_tokens=4096,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
@@ -24,3 +25,17 @@ def generate_cot(model, cot_prompt):
         return eval_steps
     except Exception as e:
         raise e
+    
+if __name__ == '__main__':
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--prompt-fp', type=str, default='prompts/dialogue/cot/coh_cot.txt')
+    argparser.add_argument('--save_fp', type=str, default='results/coh_cot_llama.json')
+    argparser.add_argument('--model', type=str, default='Llama 3 8B Instruct')
+
+    args = argparser.parse_args()
+
+    eval_steps = generate_cot(args.model, args.prompt_fp)
+
+    with open(args.save_fp, 'w') as f:
+        f.write(eval_steps)
