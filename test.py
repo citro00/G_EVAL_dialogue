8a4fbf952285caec79311a068f1236eb105de810
Cvlmtt <mattiacavaliere0114@gmail.com>
2025-01-28 16:14:12 +0100

    Ristrutturazione
    Ristrutturazione delle cartelle e dei file di input/output. Modifica dei prompt e del calcolo della probabilità

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-ds = json.load(open("data/dstc9_data.json"))
-
-num_context = len(set(ds['contexts']))
-num_response = len(set(ds['responses']))
-num_scores = len(set(ds['scores']))
-
-print(f"Context: {num_context}; Respnses: {num_response}; Scores: {num_scores}")
