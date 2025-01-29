d41440da73d6e873e6ddee53345d9bb0f5eb592e
Vincenzo <vinciosdev@gmail.com>
2025-01-29 17:17:51 +0100

    Plot: fix diagonal plot

--- plot.py
+++ plot.py
@@ -2,6 +2,20 @@ import argparse
 import json
 import matplotlib.pyplot as plt
 
+
+def add_identity(axes, *line_args, **line_kwargs):
+    identity, = axes.plot([], [], *line_args, **line_kwargs)
+    def callback(axes):
+        low_x, high_x = axes.get_xlim()
+        low_y, high_y = axes.get_ylim()
+        low = max(low_x, low_y)
+        high = min(high_x, high_y)
+        identity.set_data([low, high], [low, high])
+    callback(axes)
+    axes.callbacks.connect('xlim_changed', callback)
+    axes.callbacks.connect('ylim_changed', callback)
+    return axes
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_fp', type=str, default='results/overall_mean.json')
@@ -15,8 +29,11 @@ if __name__ == "__main__":
 
     fig, ax = plt.subplots()
     
-    ax.plot([0,1], [0,1], transform=ax.transAxes, color='red')
+    add_identity(ax, color='r', ls='--')
     ax.scatter(human_scores, predicted_scores, s=20) 
+    ax.set_xlim([0, 6])
+    ax.set_ylim([0, 6])
+
     ax.set_xlabel('Human Scores')
     ax.set_ylabel('Predicted Scores')
     ax.set_title('Human vs Predicted Scores')
